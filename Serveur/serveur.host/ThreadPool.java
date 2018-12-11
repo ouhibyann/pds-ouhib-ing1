d2b4ec0fa5ea48201079402fc859bf5c03faac89
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2018-12-11 21:07:45 +0100

    Create ThreadPool.java

---
+++ Serveur/serveur.host/ThreadPool.java
@@ -0,0 +1,29 @@
+package serveur.host;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ThreadPool implements Runnable{
+
+	private ServerSocket serversocket;
+	private Socket socket;
+	private int nb_connexions = 1;
+	
+	public ThreadPool(ServerSocket s) { //Pool de thread qui s'occupe de gérer les connexions 
+		serversocket = s; 
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				socket = serversocket.accept();
+				System.out.println("Le client numéro :" +nb_connexions+ "est connecte !");
+				nb_connexions++;
+				socket.close();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
