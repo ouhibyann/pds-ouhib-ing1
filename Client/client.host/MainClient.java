f020527690a3dbffa7970ffaf167eba4dcf49359
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2018-12-19 21:49:53 +0100

    Update MainClient.java
    Ajout de la de-sérialisation

--- Client/client.host/MainClient.java
+++ Client/client.host/MainClient.java
@@ -1,23 +1,29 @@
 package client.host;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import com.google.gson.Gson;
+
+
 public class MainClient {
 
 	public static void main(String[] args) {
-
+	
+		Gson gson = new Gson();
+		
 		try {
 			
 		    Socket socket = new Socket(InetAddress.getLocalHost(),2019); //Ici, on est en local donc l'adresse IP s'obtient via getLocalHost
 		    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		    
-		    System.out.println(in.readLine()); //on récupère ce que le serveur nous envoie 
-	        
+		
+		    String received = gson.fromJson(in, String.class);
+		    System.out.println(received); //on récupère ce que le serveur nous envoie 
 		    socket.close();
 
 		} catch (UnknownHostException e) {
