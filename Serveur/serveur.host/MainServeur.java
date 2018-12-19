e307e529ce45a75251ad35bb363735e18f3bd3e1
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2018-12-19 21:49:06 +0100

    Update MainServeur.java
    Ajout de la sérialisation

--- Serveur/serveur.host/MainServeur.java
+++ Serveur/serveur.host/MainServeur.java
@@ -5,30 +5,38 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class MainServeur {
+	
 	
 	public static void main(String[] zero) {
 		
 		
 		try {
-		
+			
+
+			Gson gson = new GsonBuilder().create(); //On initialise l'objet Json
 			ServerSocket serversocket = new ServerSocket(2019, 2); //num_port, nb_max_of_co
-			Thread t = new Thread(new ThreadPool(serversocket)); //on initialise le Pool de Thread avec notre Serveur socket
+			Thread t = new Thread(new ThreadPool(serversocket));
+			
 			t.start();
 			System.out.println("En attente de connexion");
 			Socket socketduserveur = serversocket.accept(); 
+			
 			PrintWriter out = new PrintWriter(socketduserveur.getOutputStream());
 
-			Profil p = new Profil();
-			out.write(p.getProfil());
 			
-			out.flush();    
-			serversocket.close();
-	        socketduserveur.close();
-
+			Profil p = new Profil();
+			gson.toJson(p.getProfil(), out); //Serialisation
+			
+			out.flush();
+			socketduserveur.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
