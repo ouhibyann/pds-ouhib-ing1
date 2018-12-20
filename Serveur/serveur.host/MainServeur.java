ea8a028905287aabb4274a9c0ca90af6422fe65b
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2018-12-20 22:57:51 +0100

    Update MainServeur.java
    Modification du Main en adéquation avec l'implémentation du Pool de Connexion

--- Serveur/serveur.host/MainServeur.java
+++ Serveur/serveur.host/MainServeur.java
@@ -1,42 +1,13 @@
 package serveur.host;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+//import java.net.InetAddress;
 
 public class MainServeur {
 	
-	
-	public static void main(String[] zero) {
-		
-		
-		try {
-			
-
-			Gson gson = new GsonBuilder().create(); //On initialise l'objet Json
-			ServerSocket serversocket = new ServerSocket(2019, 2); //num_port, nb_max_of_co
-			Thread t = new Thread(new ThreadPool(serversocket));
-			
-			t.start();
-			System.out.println("En attente de connexion");
-			Socket socketduserveur = serversocket.accept(); 
-			
-			PrintWriter out = new PrintWriter(socketduserveur.getOutputStream());
-
-			
-			Profil p = new Profil();
-			gson.toJson(p.getProfil(), out); //Serialisation
-			
-			out.flush();
-			socketduserveur.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public static void main(String args[]) {
+		ThreadPool tp = new ThreadPool();
+		tp.open();
+		System.out.println("Serveur initialisé");
 		
 	}
-
 }
