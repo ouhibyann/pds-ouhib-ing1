f85bf33ceaf89daccd8aad48644bf5517e2b40b5
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2019-01-28 19:40:47 +0100

    Create ConnectionPool.java
    1 ère implémentation du pool de connexion (version non définitive)

---
+++ Serveur/serveur.host/ConnectionPool.java
@@ -0,0 +1,78 @@
+package serveur.host;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionPool 
+  implements IConnectionPool {
+ 
+    private String url;
+    private String user;
+    private String password;
+    private List<Connection> connectionPool;
+    private List<Connection> usedConnections = new ArrayList<>();
+    private static int INITIAL_POOL_SIZE = 10;
+     
+    public ConnectionPool(String url, String user, String password,List<Connection> pool) {}
+    public ConnectionPool create(String url, String user, String password) throws SQLException {
+  
+        List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+            pool.add(createConnection(url, user, password));
+        }
+        return new ConnectionPool(url, user, password, pool);
+    }
+     
+    // standard constructors
+     
+    @Override
+    public Connection getConnection() {
+        Connection connection = connectionPool
+          .remove(connectionPool.size() - 1);
+        usedConnections.add(connection);
+        return connection;
+    }
+     
+    @Override
+    public boolean releaseConnection(Connection connection) {
+        connectionPool.add(connection);
+        return usedConnections.remove(connection);
+    }
+     
+    public Connection createConnection(String url, String user, String password) throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+     
+    
+    public int getSize() {
+        return connectionPool.size() + usedConnections.size();
+    }
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+ 
+    
+}
