6aa6e208cb8a7a68125702ba5d4d4af18dba570f
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2019-01-30 10:02:52 +0100

    Update ClientProcessor.java
    Problème au niveau du out.flush() qui ne fonctionne pas, utilisation du out.close() pour résoudre le soucis mais aucune solution autre trouvée.
    Implémentation d'un Thread qui décompte le nombre de Connection active dans le Pool.
    Chargement des Properties dans le Constructor pour plus de "propreté".

--- Serveur/serveur.host/ClientProcessor.java
+++ Serveur/serveur.host/ClientProcessor.java
@@ -3,6 +3,7 @@ package serveur.host;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -26,83 +27,94 @@ public class ClientProcessor extends Thread{
 	String url;
 	String user;
 	String password;
-
+	ConnectionPool c;
 
 	public ClientProcessor() {
-		
+		try {
+			config = LoadProperties.load("src/properties/config.properties");
+			Class.forName(config.getProperty("driver"));
+			this.url = config.getProperty("url");
+			this.user = config.getProperty("user");
+			this.password = config.getProperty("password");
+				
+			c = new ConnectionPool(url, user, password);
+			c.create(url, user, password);
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch(ClassNotFoundException e) {
+			e.getMessage();
+		}
 	}
 	
 	public void run() {
 	
+		PrintAvailableConnection();
 		try {
 			serversocket = new ServerSocket(2019);
 			socketduserveur = serversocket.accept();
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-		while(!socketduserveur.isClosed()) {	
-			try {
-				PrintWriter out = new PrintWriter(socketduserveur.getOutputStream());
-				Gson gson = new GsonBuilder().create(); //On initialise l'objet Json
-				BufferedReader in = new BufferedReader(new InputStreamReader(socketduserveur.getInputStream()));	
-				String received = in.readLine();
-				
-				switch(received.toLowerCase()) { //Lorsque le client demande l'envoie, on lui affiche les profils
-				
-					case "envoie":
-						
-						System.out.println("Commande envoie detectee");
-						gson.toJson(getProfil(), out); //Serialisation
-						out.flush();
-						//out.close();
-						socketduserveur.close();
-						break;
-					
-					case "close":
-						System.err.println("Commande close detectee");
-						gson.toJson("La connexion va s'arreter", out);
-						out.flush();
-						in.close();
-						out.close();
-						socketduserveur.close();
-						break;
-				}
-				
-			}catch (SocketException e) {
-				System.err.println("LA CONNEXION A ETE INTERROMPUE !");
-				break;
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
 			
-		}
-		
+			PrintWriter out = new PrintWriter(new OutputStreamWriter(socketduserveur.getOutputStream()), true);
+			Gson gson = new GsonBuilder().create(); //On initialise l'objet Json
+			BufferedReader in = new BufferedReader(new InputStreamReader(socketduserveur.getInputStream()));	
+			String received = "";
+			String tosend = "";
+			
+			while(!socketduserveur.isClosed()) {	
+
+				if (in.ready()) {
+					//received = in.readLine();
+					received = gson.fromJson(in.readLine(), String.class);
+					
+					if (received == null) {
+						socketduserveur.close();
+					}
+					
+					switch(received.toLowerCase()) { //Lorsque le client demande l'envoie, on lui affiche les profils
+						
+						case "envoie":
+							
+							System.out.println("Commande envoie detectee");
+							tosend = getProfil();
+							System.out.println(tosend);
+							
+							//out.flush();
+							//out.close();
+							//socketduserveur.close();
+							break;
+						
+						case "close":
+							System.err.println("Commande close detectee");
+							gson.toJson("La connexion va s'arreter", out);
+							out.flush();
+							in.close();
+							//out.close();
+							//socketduserveur.close();
+							break;			
+					}
+					
+					out.println(gson.toJson(tosend));
+					out.flush();
+					out.close();
+					tosend = "";
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 	
 	private String getProfil() {
 		try {
 			
 			String result = "";
-			try {
-				
-				config = LoadProperties.load("src/properties/config.properties");
-				Class.forName(config.getProperty("driver"));
-				this.url = config.getProperty("url");
-				this.user = config.getProperty("user");
-				this.password = config.getProperty("password");
-				
-				
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-			}
-			ConnectionPool c = new ConnectionPool(url, user, password);
-			c.create(url, user, password);
+
 			
 			String sql = "SELECT \"Profil\", customer_name, shop_bookmarked, customer_id\n" + 
 					"	FROM public.\"Profils\";";
 			
-			Statement st = ((java.sql.Connection) c.createConnection(c.getUrl(), c.getUser(), c.getPassword())).createStatement();
+			java.sql.Connection con = c.getConnection();
+			Statement st = con.createStatement();
 			
 			ResultSet rs = st.executeQuery(sql);
 			
@@ -116,13 +128,36 @@ public class ClientProcessor extends Thread{
 				result += profil + " " + shop_name + " " + customer_name + " " + customer_id + "\n";
 				
 			}
+			Thread.sleep(5000);
+			c.releaseConnection(con);
 			st.close();
 			return result;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+			return null;
 		}
+	}
+	
+	public void PrintAvailableConnection() {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				while(true) {
+					try {
+						System.out.println("Les connexions disponibles sont au nombre de : " + c.getSize());
+						Thread.sleep(2000);
+					} catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+		});
+		
+		t.start();
+		
 	}
 	
 }
