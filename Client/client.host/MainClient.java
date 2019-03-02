bb312db9317d76af9f9b2bf69d266347e50d569e
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2019-03-02 11:03:38 +0100

    Update MainClient.java
    Implémentation de l'IHM dans le mainClient

--- Client/client.host/MainClient.java
+++ Client/client.host/MainClient.java
@@ -1,19 +1,21 @@
 package client.host;
 
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.BufferedReader;
-
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-//import java.net.InetAddress;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import com.google.gson.Gson;
+
+import ihm.Fenetre;
 
 
 public class MainClient {
@@ -23,38 +25,41 @@ public class MainClient {
 		Gson gson = new Gson();
 		
 		try {
-			InetSocketAddress inet = new InetSocketAddress("192.168.20.13",1042);
-			Socket socket = new Socket(); //Ici, on n'est pas en local
-		    System.out.println(inet.getHostName());
-		    socket.connect(inet);
+			
+			Socket socket = new Socket(InetAddress.getLocalHost(),1042);
+			System.out.println(socket.getInetAddress());
+			//InetSocketAddress inet = new InetSocketAddress("192.168.20.13",1042);
+			//Socket socket = new Socket(); //Ici, on n'est pas en local donc l'adresse IP s'obtient via getLocalHost
+		    //System.out.println(inet.getHostName());
+		    //socket.connect();
 			
 		    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		    PrintWriter out = new PrintWriter(socket.getOutputStream());
-		    Scanner sc  = new Scanner(System.in);
-		    
-				while (!socket.isClosed()) {
-			    
-			    System.out.println("Passez votre commande"); 
-			    String commande = sc.nextLine(); // On demande au serveur de nous envoyer les profils
-			    out.write(commande +"\n");
-			    out.flush();
-			    
-			    String received = gson.fromJson(in, String.class); //On desérialise
-			    System.out.println(received); //on récupère ce que le serveur nous envoie 
-			    
-			    //sc.close();
-			    if (received == null) {
-			    	//System.out.println("fin");
-			    	sc.close();
-			    	socket.close();
-			    	}
+		    Fenetre f = new Fenetre();
+
+		    f.getJbProfil().addMouseListener(new MouseListener() {
+		    	
+		    	public void mouseReleased(MouseEvent e) {}
+				public void mousePressed(MouseEvent e) {}
+				public void mouseExited(MouseEvent e) {}
+				public void mouseEntered(MouseEvent e) {}
+				
+				public void mouseClicked(MouseEvent e) {
+
+					out.write("envoie \n");
+					out.flush();
+				    String received = gson.fromJson(in, String.class); //On desérialise
+					f.getshowProfil().setText(received);
+					
 				}
+		    	
+		    });
 				
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		} 
 	}
 
 }
