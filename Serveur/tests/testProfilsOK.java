c59d2844c55f755fef5a3c132a7237b439dfe0d4
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2019-04-09 17:58:42 +0200

    Update testProfilsOK.java
    Ajouts de commentaires

--- Serveur/tests/testProfilsOK.java
+++ Serveur/tests/testProfilsOK.java
@@ -3,15 +3,15 @@ package tests;
 import serveur.host.ClientProcessor;
 import serveur.host.Profils;
 
-class testProfilsOK {
+class testProfilsKO {
 
+	//checks that getProfil is OK
 	public static void main(String[] args) {
 		
-		//Should return a correct profil
+		//Should return an error because there is no client with the id 0
 		ClientProcessor cp = new ClientProcessor();
-		Profils p = new Profils(4,cp.getC());
+		Profils p = new Profils(0,cp.getC());
 		System.out.println(p.getProfil());
-		
 	}
 
 }
