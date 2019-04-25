cda9112b72b14a935d683065d888206e0f037dff
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2019-04-25 19:42:51 +0200

    Update Delete_Purchases.java
    Modifications en accord avec le pool de connections

--- Serveur/Scripts/Delete_Purchases.java
+++ Serveur/Scripts/Delete_Purchases.java
@@ -3,9 +3,7 @@ package Scripts;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Properties;
 
-import properties.LoadProperties;
 import serveur.host.ConnectionPool;
 
 
@@ -15,23 +13,11 @@ public class Delete_Purchases {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Properties config;
-		String url;
-		String user;
-		String password;
-		
 		try {
-			
-			config = LoadProperties.load("src/properties/config.properties");
-			Class.forName(config.getProperty("driver"));
-			url = config.getProperty("url");
-			user = config.getProperty("user");
-			password = config.getProperty("password");
 			
 			String sql = "TRUNCATE TABLE public.purchase";
 		   
-			ConnectionPool pool = new ConnectionPool(url,user,password);
-			pool.create(url, user, password);
+			ConnectionPool pool = new ConnectionPool();
 			Connection conn = pool.getConnection();
 			PreparedStatement st = conn.prepareStatement(sql);
 			st.executeQuery();
