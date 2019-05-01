9fc64a3e93e49bd12778da1921abd1e867a01358
yann ouhib <34911252+ouhibyann@users.noreply.github.com>
2019-05-01 18:49:05 +0200

    Update MainServeur.java
    Refactoring du code

--- Serveur/serveur.host/MainServeur.java
+++ Serveur/serveur.host/MainServeur.java
@@ -1,14 +1,33 @@
 package serveur.host;
 
-//import java.net.InetAddress;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.sql.SQLException;
 
 public class MainServeur {
-	
-	public static void main(String args[]) {
-		System.out.println("Serveur initialisé");
-		ClientProcessor cp = new ClientProcessor();
-		cp.create();
-		cp.run();
-	}
-	
+
+    private ServerSocket socketserver;
+
+    //Init the server here is easier for multiThreading handling
+    private MainServeur() throws IOException, SQLException, ClassNotFoundException {
+        socketserver = new ServerSocket(1042);
+        DAO.initPool();
+    }
+
+    //we create a seperated thread for each client
+    private void AcceptConnection() throws IOException {
+            new Thread(new ClientProcessor(this.socketserver.accept())).start();
+    }
+
+    //accept until the server socket is closed, which should never happen
+    private void run() throws IOException {
+        while(!socketserver.isClosed()) AcceptConnection();
+    }
+
+    //launching the server
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        System.out.println("Le serveur est initialisé");
+    	new MainServeur().run();
+    }
 }
